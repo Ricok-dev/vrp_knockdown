fb3727aed18e9cc709541e929167eecb6d56be65
Ricok-dev <53659348+Ricok-dev@users.noreply.github.com>
2019-10-02 14:22:24 +0200

    Update hotkeys.lua

--- cfg/hotkeys.lua
+++ cfg/hotkeys.lua
@@ -1,71 +1,32 @@
 -- TUNNEL AND PROXY
 cfg = {}
 vRPhk = {}
-Tunnel.bindInterface("vrp_hotkeys",vRPhk)
-vRPserver = Tunnel.getInterface("vRP","vrp_hotkeys")
-HKserver = Tunnel.getInterface("vrp_hotkeys","vrp_hotkeys")
+Tunnel.bindInterface("vrp_knockdown",vRPhk)
+vRPserver = Tunnel.getInterface("vRP","vrp_knockdown")
+HKserver = Tunnel.getInterface("vrp_knockdown","vrp_knockdown")
 vRP = Proxy.getInterface("vRP")
 
 -- GLOBAL VARIABLES
 called = 0
 
--- YOU ARE ON A CLIENT SCRIPT ( Just reminding you ;) )
--- Keys IDs can be found at https://wiki.fivem.net/wiki/Controls
 
 
--- Hotkeys Configuration: cfg.hotkeys = {[Key] = {group = 1, pressed = function() end, released = function() end},}
+
+
+
 cfg.hotkeys = {
-  [46] = {
-    -- E call/skip emergency
-    group = 0, 
-	pressed = function() 
-	  if vRP.isInComa({}) then
-	    if called == 0 then
-	      HKserver.canSkipComa({},function(skipper)
-		    if skipper then
-		      HKserver.docsOnline({},function(docs)
-		        if docs == 0 then
-				  vRP.killComa({})
-			    else
-				  called = 30
-				  local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1),true))
-				  HKserver.helpComa({x,y,z})
-			    end
-			  end)
-            end
-		  end)
-		else
-		  vRP.notify({"~r~Acabas de llamar a la ambulancia."})
-		end
-	  end
-	end,
-	released = function()
-	  -- Do nothing on release because it's toggle.
-	end,
-	},
-
- 	[168] = {
-    -- F2 toggle Cuff nearest player
+  [58] = {
+    -- G noquea si tiene arma
     group = 1, 
 	pressed = function() 
-      if not IsPauseMenuActive() and not IsPedInAnyVehicle(GetPlayerPed(-1), true) then -- Comment to allow use in vehicle
-	    HKserver.toggleHandcuff()
-	  end -- Comment to allow use in vehicle
+      if not IsPauseMenuActive() and  IsPedArmed(GetPlayerPed(-1), 7) then 
+		HKserver.nockout()
+	  else 
+		vRP.notify({"~r~No tienes un arma contundente."})
+	  end 
 	end,
 	released = function()
 	  -- Do nothing on release because it's toggle.
 	end,
   },
-  
-  
-   --[[ [213] = {
-    --home on numberpad toggle User List
-    group = 0, 
-	pressed = function() 
-	  HKserver.openUserList({})
-	end,
-	released = function()
-	  -- Do nothing on release because it's toggle.
-	end,
-  },]]
 }
